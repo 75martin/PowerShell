c54d664b2c0789ad458cd20c43be0db644c997ba
75martin <gustavsson.j.m@gmail.com>
2018-03-13 09:10:57 +0100

    Add files via upload

---
+++ keyb_numlock.ps1
@@ -0,0 +1,23 @@
+﻿#################################################################
+# 2nd method:                                                   #
+# 1. Shutdown computer and start it again.                      #
+# 2. At logon screen, enable numlock and shutdown.              #
+# 3. Start computer back up and login.                          #
+#################################################################
+
+
+
+Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power" -Name HiberbootEnabled -Value 0 -Verbose
+
+#$tp = Test-Path HKU:
+#if ($tp -eq $false){
+#    New-PSDrive -Name HKU -PSProvider Registry -Root HKEY_USERS
+#    }
+
+#Set-ItemProperty "HKU:\*\Control Panel\Keyboard" -Name InitialKeyboardIndicators -Value 2 -Verbose
+#Set-ItemProperty "HKCU:\Control Panel\Keyboard" -Name InitialKeyboardIndicators -Value 2 -Verbose
+
+Set-ExecutionPolicy Restricted
+$ExPo = Get-ExecutionPolicy
+Write-Host "ExecutionPolicy set to $ExPo"
+pause
